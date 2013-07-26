85feeeb6d7199b41d9ddba291ee01032308cbb32
Patrick <patrick.harold.kelly@gmail.com>
2013-07-25 22:26:32 -0400

    Added spans inside menu a hrefs so that I can center vertically the menu items. Made more changes to css to help layout

--- public/js/layout-adjustments.js
+++ public/js/layout-adjustments.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $('.nav-tabs a').each(function () {
-        var linklabel = $(this).text();
-        if(linklabel.length < 8) {
-            $(this).css('line-height','50px' )
+    $('.nav-tabs a span').each(function () {
+        var h = $(this).height();
+        if(h < 46) {
+            $(this).css('line-height','50px');
         }
     });
 });
